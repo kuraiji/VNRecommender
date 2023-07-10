b6f07b5d91ec1d8b028712df79b4543eb185011b
trenton-coggeshall <90164941+trenton-coggeshall@users.noreply.github.com>
2023-07-10 10:10:35 -0700

    Test

---
+++ Frontend/VNDB_Recommender/src/pages/Test.tsx
@@ -0,0 +1,17 @@
+import { Button, Flex, Group, Text } from "@mantine/core";
+
+function Test() {
+
+    return (
+        <Flex justify={"center"} direction={"column"} align={"center"}>
+            <p>This is a test page!</p>
+            <button>
+                This is a button
+            </button>
+
+        </Flex>
+
+    );
+}
+
+export default Test;
